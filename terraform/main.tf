3772706fb254f9afa7aa82b0a7264e4add511731
KostasKoutrou <32481775+KostasKoutrou@users.noreply.github.com>
2026-01-28 15:12:33 +0200

    Terraform added few proxmox default configs so that terraform doesn't try to reconfigure them at every terraform apply

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,6 +26,13 @@ resource "proxmox_vm_qemu" "test_server" {
         size = "20G"
         type = "disk"
         storage = "local-lvm"
+        format = "raw"
+    }
+
+    startup_shutdown {
+        order = -1
+        shutdown_timeout = -1
+        startup_delay = -1
     }
 
     network {
@@ -39,43 +46,50 @@ resource "proxmox_vm_qemu" "test_server" {
     skip_ipv6 = true
 }
 
-# resource "proxmox_vm_qemu" "test_server1" {
-#     name = "terraform-vm-02"
-#     target_node = "kkproxmox"
+resource "proxmox_vm_qemu" "test_server1" {
+    name = "terraform-vm-02"
+    target_node = "kkproxmox"
 
-#     clone = "ubuntu-2404-template"
+    clone = "ubuntu-2404-template"
 
-#     agent = 1 #enable QEMU guest agent
+    agent = 1 #enable QEMU guest agent
     
-#     cpu {
-#         cores = 2
-#         sockets = 1
-#         type = "host"
-#     }
-#     memory = 2048
-#     scsihw = "virtio-scsi-pci"
-#     # bootdisk = "scsi0"
+    cpu {
+        cores = 2
+        sockets = 1
+        type = "host"
+    }
+    memory = 2048
+    scsihw = "virtio-scsi-pci"
+    # bootdisk = "scsi0"
 
-#     disk {
-#         slot    = "ide0"
-#         type    = "cloudinit"
-#         storage = "local-lvm"
-#     }
+    disk {
+        slot    = "ide0"
+        type    = "cloudinit"
+        storage = "local-lvm"
+    }
 
-#     disk {
-#         slot = "scsi0"
-#         size = "20G"
-#         type = "disk"
-#         storage = "local-lvm"
-#     }
+    disk {
+        slot = "scsi0"
+        size = "20G"
+        type = "disk"
+        storage = "local-lvm"
+        format = "raw"
+    }
 
-#     network {
-#         id = 0
-#         model = "virtio"
-#         bridge = "vmbrEUZ40"
-#     }
+    startup_shutdown {
+        order = -1
+        shutdown_timeout = -1
+        startup_delay = -1
+    }
 
-#     os_type = "cloud-init"
-#     ipconfig0 = "ip=dhcp"
-#     skip_ipv6 = true
-# }
+    network {
+        id = 0
+        model = "virtio"
+        bridge = "vmbrEUZ40"
+    }
+
+    os_type = "cloud-init"
+    ipconfig0 = "ip=dhcp"
+    skip_ipv6 = true
+}
