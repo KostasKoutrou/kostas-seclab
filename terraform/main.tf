b74b95a7f94e058b7de600b78a8a93a207c60dd9
KostasKoutrou <32481775+KostasKoutrou@users.noreply.github.com>
2026-03-08 11:47:34 +0200

    opnsense ansible configure DHCP, cleanup secrets

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,101 +1,101 @@
-# resource "proxmox_vm_qemu" "test_server" {
-#     name = "terraform-vm-01"
-#     target_node = "kkproxmox"
+resource "proxmox_vm_qemu" "test_server" {
+    name = "terraform-vm-01"
+    target_node = "kkproxmox"
 
-#     clone = "ubuntu-2404-template"
+    clone = "ubuntu-2404-template"
 
-#     agent = 1 #enable QEMU guest agent
+    agent = 1 #enable QEMU guest agent
     
-#     cpu {
-#         cores = 2
-#         sockets = 1
-#         type = "host"
-#     }
-#     memory = 2048
-#     scsihw = "virtio-scsi-pci"
-#     # bootdisk = "scsi0"
+    cpu {
+        cores = 2
+        sockets = 1
+        type = "host"
+    }
+    memory = 2048
+    scsihw = "virtio-scsi-pci"
+    # bootdisk = "scsi0"
 
-#     # This is addeded to the default ciuser "ubuntu". ssh-keygen -t rsa to generate the key pair.
-#     sshkeys = "${file("~/.ssh/id_rsa.pub")}"
+    # This is addeded to the default ciuser "ubuntu". ssh-keygen -t rsa to generate the key pair.
+    sshkeys = "${file("~/.ssh/id_rsa.pub")}"
 
-#     disk {
-#         slot    = "ide0"
-#         type    = "cloudinit"
-#         storage = "local-lvm"
-#     }
+    disk {
+        slot    = "ide0"
+        type    = "cloudinit"
+        storage = "local-lvm"
+    }
 
-#     disk {
-#         slot = "scsi0"
-#         size = "20G"
-#         type = "disk"
-#         storage = "local-lvm"
-#         format = "raw"
-#     }
+    disk {
+        slot = "scsi0"
+        size = "20G"
+        type = "disk"
+        storage = "local-lvm"
+        format = "raw"
+    }
 
-#     startup_shutdown {
-#         order = -1
-#         shutdown_timeout = -1
-#         startup_delay = -1
-#     }
+    startup_shutdown {
+        order = -1
+        shutdown_timeout = -1
+        startup_delay = -1
+    }
 
-#     network {
-#         id = 0
-#         model = "virtio"
-#         bridge = "vmbr0"
-#     }
+    network {
+        id = 0
+        model = "virtio"
+        bridge = "vmbr0"
+    }
 
-#     os_type = "cloud-init"
-#     ipconfig0 = "ip=dhcp"
-#     skip_ipv6 = true
-# }
+    os_type = "cloud-init"
+    ipconfig0 = "ip=dhcp"
+    skip_ipv6 = true
+}
 
-# resource "proxmox_vm_qemu" "test_server1" {
-#     name = "terraform-vm-02"
-#     target_node = "kkproxmox"
+resource "proxmox_vm_qemu" "test_server1" {
+    name = "terraform-vm-02"
+    target_node = "kkproxmox"
 
-#     clone = "ubuntu-2404-template"
+    clone = "ubuntu-2404-template"
 
-#     agent = 1 #enable QEMU guest agent
+    agent = 1 #enable QEMU guest agent
     
-#     cpu {
-#         cores = 2
-#         sockets = 1
-#         type = "host"
-#     }
-#     memory = 2048
-#     scsihw = "virtio-scsi-pci"
-#     # bootdisk = "scsi0"
+    cpu {
+        cores = 2
+        sockets = 1
+        type = "host"
+    }
+    memory = 2048
+    scsihw = "virtio-scsi-pci"
+    # bootdisk = "scsi0"
 
-#     disk {
-#         slot    = "ide0"
-#         type    = "cloudinit"
-#         storage = "local-lvm"
-#     }
+    disk {
+        slot    = "ide0"
+        type    = "cloudinit"
+        storage = "local-lvm"
+    }
 
-#     disk {
-#         slot = "scsi0"
-#         size = "20G"
-#         type = "disk"
-#         storage = "local-lvm"
-#         format = "raw"
-#     }
+    disk {
+        slot = "scsi0"
+        size = "20G"
+        type = "disk"
+        storage = "local-lvm"
+        format = "raw"
+    }
 
-#     startup_shutdown {
-#         order = -1
-#         shutdown_timeout = -1
-#         startup_delay = -1
-#     }
+    startup_shutdown {
+        order = -1
+        shutdown_timeout = -1
+        startup_delay = -1
+    }
 
-#     network {
-#         id = 0
-#         model = "virtio"
-#         bridge = "vmbrEUZ40"
-#     }
+    network {
+        id = 0
+        model = "virtio"
+        bridge = "vmbrEUZ40"
+    }
 
-#     os_type = "cloud-init"
-#     ipconfig0 = "ip=dhcp"
-#     skip_ipv6 = true
-# }
+    os_type = "cloud-init"
+    ipconfig0 = "ip=dhcp"
+    skip_ipv6 = true
+}
 
 resource "proxmox_vm_qemu" "c-opnsense" {
     name = "c-opnsense"
